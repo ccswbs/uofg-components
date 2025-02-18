002935d050539cf75742d5b3616e6cf5fd655f29
Raza Mahmood <razammahmood@gmail.com>
2025-02-17 23:47:37 -0500

    Add Card component to exports in react-components index
    This update includes the Card component in the exports of the react-components package. This ensures that the Card component can now be imported and used externally where needed.

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -5,5 +5,6 @@ import { Button } from './components/button.tsx';
 import { Blockquote } from './components/blockquote.tsx';
 import { Breadcrumbs } from './components/breadcrumbs.tsx';
 import { Container } from './components/container.tsx';
+import { Card } from './components/card.tsx';
 
-export { Accordion, Alert, Button, Blockquote, Breadcrumbs, Container };
+export { Accordion, Alert, Button, Blockquote, Breadcrumbs, Container, Card };
