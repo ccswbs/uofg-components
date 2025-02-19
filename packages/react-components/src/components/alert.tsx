4145d4da81f3e82ce088c5088d80d117c7036455
Raza Mahmood <razammahmood@gmail.com>
2025-02-18 22:46:53 -0500

    Refactor Alert component to use tailwind-variants.
    Replaced Tailwind-merge with Tailwind-variants for improved styling flexibility and maintainability. Expanded color options and restructured the component for better reuse and clarity. This enhances customization while keeping the codebase cleaner.

--- packages/react-components/src/components/alert.tsx
+++ packages/react-components/src/components/alert.tsx
@@ -1,36 +1,80 @@
 import type { FC, ReactNode } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleExclamation } from '@awesome.me/kit-7993323d0c/icons/classic/regular';
-import { twJoin } from 'tailwind-merge';
+import { tv } from 'tailwind-variants';
 
 export type AlertProps = {
-  type?: 'danger' | 'warning' | 'info';
+  color?: 'red' | 'yellow' | 'blue' | 'green' | 'grey' | 'black' | 'white';
   title: ReactNode;
   subtitle: ReactNode;
   message: ReactNode;
   footer?: ReactNode;
 };
-export const Alert: FC<AlertProps> = ({ type = 'danger', title, subtitle, message, footer }) => (
-  <div className="flex flex-col">
-    <div
-      className={twJoin(
-        'flex items-center gap-2 p-4 text-xl',
-        type === 'danger' && 'bg-red text-white',
-        type === 'warning' && 'bg-yellow text-black',
-        type === 'info' && 'bg-blue text-white',
-      )}
-    >
-      <FontAwesomeIcon className="h-[1.5em]" icon={faCircleExclamation} />
-      <span>{title}</span>
-    </div>
+export const Alert: FC<AlertProps> = ({ color = 'red', title, subtitle, message, footer }) => {
+  const card = tv({
+    slots: {
+      base: 'flex flex-col',
+      titleContainer: 'flex items-center gap-2 p-4 text-xl',
+      titleIcon: 'h-[1.5em]',
+      titleText: '',
+      messageContainer: 'flex flex-col border-x border-b border-grey-light bg-white px-4 py-3',
+      subtitle: 'font-bold mb-4 text-xl',
+      message: 'text-lg',
+      footer: 'flex bg-grey-light px-4 py-2',
+    },
+    variants: {
+      color: {
+        red: {
+          titleContainer: 'bg-red text-red-contrast',
+        },
+        yellow: {
+          titleContainer: 'bg-yellow text-yellow-contrast',
+        },
+        blue: {
+          titleContainer: 'bg-blue text-blue-contrast',
+        },
+        green: {
+          titleContainer: 'bg-green text-green-contrast',
+        },
+        grey: {
+          titleContainer: 'bg-grey text-grey-contrast',
+        },
+        black: {
+          titleContainer: 'bg-black text-black-contrast',
+        },
+        white: {
+          titleContainer: 'bg-white text-white-contrast',
+        },
+      },
+    },
+  });
 
-    <div className={`flex flex-col border-x border-b border-grey-light bg-white px-4 py-3`}>
-      <span className="font-bold mb-4 text-xl">{subtitle}</span>
-      <span className="text-lg">{message}</span>
-    </div>
+  const {
+    base,
+    titleContainer,
+    titleIcon,
+    titleText,
+    messageContainer,
+    message: messageClasses,
+    subtitle: subtitleClasses,
+    footer: footerClasses,
+  } = card({ color });
 
-    {footer && <div className="flex bg-grey-light px-4 py-2">{footer}</div>}
-  </div>
-);
+  return (
+    <div className={base()}>
+      <div className={titleContainer()}>
+        <FontAwesomeIcon className={titleIcon()} icon={faCircleExclamation} />
+        <span className={titleText()}>{title}</span>
+      </div>
+
+      <div className={messageContainer()}>
+        <span className={subtitleClasses()}>{subtitle}</span>
+        <span className={messageClasses()}>{message}</span>
+      </div>
+
+      {footer && <div className={footerClasses()}>{footer}</div>}
+    </div>
+  );
+};
 
 Alert.displayName = 'Alert';
