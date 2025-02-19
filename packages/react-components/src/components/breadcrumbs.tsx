7a9d27d9b8a503a6e9a7b30f137beca15c695dea
Raza Mahmood <razammahmood@gmail.com>
2025-02-18 23:02:32 -0500

    Refactor Breadcrumbs to use Tailwind Variants (tv).
    Replaced static class names with Tailwind Variants for better organization and reusability. This improves readability and simplifies future styling adjustments.

--- packages/react-components/src/components/breadcrumbs.tsx
+++ packages/react-components/src/components/breadcrumbs.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@awesome.me/kit-7993323d0c/icons/sharp/light';
 import { faChevronRight } from '@awesome.me/kit-7993323d0c/icons/classic/light';
 import { Container } from './container';
+import { tv } from 'tailwind-variants';
 
 export type Breadcrumb = {
   title: string;
@@ -17,25 +18,35 @@ export type BreadcrumbsProps = {
 export const Breadcrumbs: FC<BreadcrumbsProps> = ({ links, as = 'a' }) => {
   const Link = as;
 
+  const breadcrumbs = tv({
+    slots: {
+      base: 'flex w-full flex-wrap items-center gap-2',
+      homeIcon: 'h-[1em] fill-black',
+      breadcrumbContainer: 'flex items-center gap-2',
+      breadcrumbIcon: 'h-[.75em]',
+      breadcrumbLink:
+        'underline decoration-transparent decoration-1 transition-colors hover:decoration-black focus:decoration-black',
+    },
+  });
+
+  const { base, homeIcon, breadcrumbContainer, breadcrumbIcon, breadcrumbLink } = breadcrumbs();
+
   return (
     <Container centered>
-      <ol className="flex w-full flex-wrap items-center gap-2">
+      <ol className={base()}>
         <li>
           <Link href="/">
-            <FontAwesomeIcon icon={faHome} className="h-[1em] fill-black" />
+            <FontAwesomeIcon icon={faHome} className={homeIcon()} />
             <span className="sr-only">U of G Homepage</span>
           </Link>
         </li>
         {links?.map((link, index) => (
-          <li key={index} className="flex items-center gap-2">
-            <FontAwesomeIcon icon={faChevronRight} className="h-[.75em]" />
+          <li key={index} className={breadcrumbContainer()}>
+            <FontAwesomeIcon icon={faChevronRight} className={breadcrumbIcon()} />
             {index === links.length - 1 ? (
               <span>{link.title}</span>
             ) : (
-              <Link
-                className="underline decoration-transparent decoration-1 transition-colors hover:decoration-black focus:decoration-black"
-                href={link.url}
-              >
+              <Link className={breadcrumbLink()} href={link.url}>
                 {link.title}
               </Link>
             )}
