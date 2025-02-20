518a96918d196935b4997c78a8213110605986e2
Raza Mahmood <razammahmood@gmail.com>
2025-02-19 23:33:32 -0500

    Enhance Accordion component styles for better usability

--- packages/react-components/src/components/accordion.tsx
+++ packages/react-components/src/components/accordion.tsx
@@ -14,14 +14,15 @@ export function Accordion({ title, children }: AccordionProps) {
     slots: {
       base: 'tw:my-2 tw:[&_p:last-child]:mb-0',
       button:
-        'tw:mb-1 tw:inline-flex tw:w-full tw:cursor-pointer tw:items-center tw:justify-between tw:gap-2 tw:p-2.5 tw:px-5 tw:text-left tw:text-xl tw:transition-colors tw:focus:outline-none tw:focus-visible:ring-2 tw:focus-visible:ring-blue tw:focus-visible:ring-offset-2 tw:hocus:bg-blue tw:hocus:text-blue-contrast',
-      icon: 'tw:h-[1em] tw:text-yellow tw:transition-transform',
+        'tw:group tw:mb-1 tw:inline-flex tw:w-full tw:cursor-pointer tw:items-center tw:justify-between tw:gap-2 tw:p-2.5 tw:px-5 tw:text-left tw:text-xl tw:transition-colors tw:focus:outline-none tw:focus-visible:ring-2 tw:focus-visible:ring-blue tw:focus-visible:ring-offset-2 tw:hocus:bg-blue tw:hocus:text-blue-contrast',
+      icon: 'tw:h-[1em] tw:text-blue-on-light tw:group-hocus:text-blue-contrast tw:transition',
       panel: 'tw:border-l-4 tw:border-yellow tw:py-3 tw:pl-5',
     },
     variants: {
       open: {
         true: {
           button: 'tw:bg-blue tw:text-blue-contrast',
+          icon: 'tw:text-blue-contrast',
         },
         false: {
           icon: 'tw:rotate-90',
