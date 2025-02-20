dbd01d62d89f81a93a7021ffad807b6439f479cf
Raza Mahmood <razammahmood@gmail.com>
2025-02-20 11:21:48 -0500

    Adjust Divider height for improved visual alignment

--- packages/react-components/src/components/divider.tsx
+++ packages/react-components/src/components/divider.tsx
@@ -2,5 +2,5 @@
  * Separates content on a page visually
  */
 export const Divider = () => (
-  <hr className="tw:mx-auto tw:my-7 tw:h-2 tw:w-3/4 tw:border-0 tw:bg-[linear-gradient(90deg,_var(--tw-color-black)_0%,_var(--tw-color-black)_33%,_var(--tw-color-red)_33%,_var(--tw-color-red)_66%,_var(--tw-color-yellow)_66%,_var(--tw-color-yellow)_100%)]" />
+  <hr className="tw:mx-auto tw:my-7 tw:h-1 tw:w-3/4 tw:border-0 tw:bg-[linear-gradient(90deg,_var(--tw-color-black)_0%,_var(--tw-color-black)_33%,_var(--tw-color-red)_33%,_var(--tw-color-red)_66%,_var(--tw-color-yellow)_66%,_var(--tw-color-yellow)_100%)]" />
 );
