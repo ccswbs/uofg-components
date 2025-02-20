62621aea75391201f17749adcc7655bd11f83dbf
Raza Mahmood <razammahmood@gmail.com>
2025-02-20 00:07:34 -0500

    Add argTypes and new story to Contact component
    Introduced argTypes for the Contact component to improve documentation and control in Storybook. Added a new "NoExtension" story to showcase contact details without a phone number extension. Adjusted formatting for consistency.

--- packages/storybook/stories/react-components/contact.stories.tsx
+++ packages/storybook/stories/react-components/contact.stories.tsx
@@ -1,17 +1,59 @@
 import * as React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { Checkbox, Contact } from '@uoguelph/react-components';
+import { Contact } from '@uoguelph/react-components';
 
 const config: Meta<typeof Contact> = {
-  title: "React Components/Contact",
+  title: 'React Components/Contact',
   component: Contact,
   parameters: {
-    layout: "centered",
+    layout: 'centered',
     docs: {
       toc: true,
     },
   },
-  tags: ["autodocs"],
+  tags: ['autodocs'],
+  argTypes: {
+    name: {
+      name: 'name',
+      description: 'The name of the contact',
+      table: {
+        type: { summary: 'string' },
+      },
+      control: {
+        type: 'text',
+      },
+    },
+    title: {
+      name: 'title',
+      description: 'The title of the contact',
+      table: {
+        type: { summary: 'string' },
+      },
+      control: {
+        type: 'text',
+      },
+    },
+    phone: {
+      name: 'phone',
+      description: 'The phone number for the contact',
+      table: {
+        type: { summary: 'string | { number: string, extension: string }' },
+      },
+      control: {
+        type: 'object',
+      },
+    },
+    email: {
+      name: 'email',
+      description: 'The email for the contact',
+      table: {
+        type: { summary: 'string' },
+      },
+      control: {
+        type: 'text',
+      },
+    },
+  },
 };
 
 export default config;
@@ -19,9 +61,18 @@ export default config;
 type Story = StoryObj<typeof Contact>;
 export const Default: Story = {
   args: {
-    name: "Example Name",
-    title: "Example Title",
-    phone: { number: '123-456-7890', extension: "12345", },
-    email: "example@example.com",
+    name: 'Example Name',
+    title: 'Example Title',
+    phone: { number: '123-456-7890', extension: '12345' },
+    email: 'example@example.com',
+  },
+};
+
+export const NoExtension: Story = {
+  args: {
+    name: 'Example Name',
+    title: 'Example Title',
+    phone: '123-456-7890',
+    email: 'example@example.com',
   },
 };
