c9301698631c14a0c8b5e44a985d2fd086664c85
Raza Mahmood <razammahmood@gmail.com>
2025-03-03 16:55:59 -0500

    Refactor Heading component to enhance prop documentation and apply a default class name for improved styling

--- packages/react-components/src/components/heading.tsx
+++ packages/react-components/src/components/heading.tsx
@@ -2,30 +2,32 @@ import { twMerge } from 'tailwind-merge';
 import { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
 import { tv } from 'tailwind-variants';
 
-const defaultElement = 'h1';
-
-type HeadingElementType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span';
-
-type HeadingPropsAs<T extends HeadingElementType> = {
-  as?: T;
-};
-
-type HeadingPropsBase = {
-  level: 1 | 2 | 3 | 4 | 5 | 6;
-  className?: string;
-};
-
-export type HeadingProps<T extends HeadingElementType = typeof defaultElement> = PropsWithChildren<
-  HeadingPropsAs<T> & ComponentPropsWithoutRef<T> & HeadingPropsBase
+export type HeadingElementType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span';
+export type HeadingProps<T extends HeadingElementType = 'h1'> = PropsWithChildren<
+  {
+    /**
+     * The element to render the heading as.
+     * @default 'h1'
+     */
+    as?: T;
+    /**
+     * The heading level.
+     */
+    level: 1 | 2 | 3 | 4 | 5 | 6;
+    /**
+     * Additional classes to apply to the heading.
+     */
+    className?: string;
+  } & ComponentPropsWithoutRef<T>
 >;
 
-export function Heading<T extends HeadingElementType = typeof defaultElement>({
+export function Heading<T extends HeadingElementType = 'h1'>({
   level,
   children,
   className,
   as,
   ...rest
-}: HeadingProps) {
+}: HeadingProps<T>) {
   const Tag = as ?? `h${level}`;
 
   const heading = tv({
@@ -43,7 +45,7 @@ export function Heading<T extends HeadingElementType = typeof defaultElement>({
   });
 
   return (
-    <Tag {...rest} className={twMerge(heading({ level }), className)}>
+    <Tag {...rest} className={`uofg-heading ${twMerge(heading({ level }), className)}`}>
       {children}
     </Tag>
   );
