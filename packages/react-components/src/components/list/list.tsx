9d99b5568d8f8fc2adae71bbeeea2443f4c80d72
Raza Mahmood <razammahmood@gmail.com>
2025-03-04 01:31:28 -0500

    Update list style

--- packages/react-components/src/components/list/list.tsx
+++ packages/react-components/src/components/list/list.tsx
@@ -18,7 +18,7 @@ export type ListProps<T extends ListElementType = typeof defaultElement> = Props
   ListPropsAs<T> & ComponentPropsWithoutRef<T> & ListPropsBase
 >;
 
-const ListContext = createContext<{ as: ListElementType; nested: boolean } | null>(null);
+const ListContext = createContext<{ nested: boolean } | null>(null);
 
 export function List<T extends ListElementType = typeof defaultElement>({
   as,
@@ -30,23 +30,21 @@ export function List<T extends ListElementType = typeof defaultElement>({
   const context = useContext(ListContext);
 
   const list = tv({
-    base: 'tw:flex tw:flex-col tw:gap-1 tw:pl-8 tw:relative tw:h-fit tw:w-full',
+    base: 'tw:flex tw:flex-col tw:gap-1 tw:relative tw:h-fit tw:w-full tw:list-inside',
     variants: {
       as: {
-        ol: 'tw:gap-2 tw:[counter-reset:list_1]',
-        ul: '',
+        ol: 'tw:gap-2 tw:list-decimal',
+        ul: 'tw:list-disc',
       },
       nested: {
-        true: 'tw:mt-0',
+        true: 'tw:ml-4',
       },
     },
   });
 
   return (
-    <Component {...rest} className={twMerge(list({ as: context?.as, nested: context?.nested }), className)}>
-      <ListContext.Provider value={{ as: Component, nested: typeof context === 'object' }}>
-        {children}
-      </ListContext.Provider>
+    <Component {...rest} className={twMerge(list({ as: Component, nested: context?.nested }), className)}>
+      <ListContext.Provider value={{ nested: typeof context === 'object' }}>{children}</ListContext.Provider>
     </Component>
   );
 }
@@ -57,20 +55,8 @@ type ListItemProps = PropsWithChildren<{
   ComponentPropsWithoutRef<'li'>;
 
 export function ListItem({ className, children, ...rest }: ListItemProps) {
-  const context = useContext(ListContext);
-
-  const listItem = tv({
-    base: 'tw:relative tw:h-fit tw:w-full tw:pl-8 tw:before:absolute tw:before:shrink-0 tw:before:items-center tw:before:justify-center tw:has-[ol]:before:content-none! tw:has-[ul]:before:content-none!',
-    variants: {
-      as: {
-        ul: 'tw:before:top-[0.35rem] tw:before:left-[.5rem] tw:before:block tw:before:h-[1.8rem] tw:before:w-[1.1rem] tw:before:text-yellow tw:before:content-[url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMjAgNTEyIj48cGF0aCBmaWxsPScjZmZjNDI5JyBkPSJNMzEwLjYgMjMzLjRjMTIuNSAxMi41IDEyLjUgMzIuOCAwIDQ1LjNsLTE5MiAxOTJjLTEyLjUgMTIuNS0zMi44IDEyLjUtNDUuMyAwcy0xMi41LTMyLjggMC00NS4zTDI0Mi43IDI1NiA3My40IDg2LjZjLTEyLjUtMTIuNS0xMi41LTMyLjggMC00NS4zczMyLjgtMTIuNSA0NS4zIDBsMTkyIDE5MnoiLz48L3N2Zz4=)]',
-        ol: 'tw:before:font-black tw:before:left-0 tw:before:inline-flex tw:before:h-6 tw:before:w-6 tw:before:bg-yellow tw:before:text-black tw:[counter-increment:list_1] tw:before:content-[counter(list)]',
-      },
-    },
-  });
-
   return (
-    <li {...rest} className={twMerge(listItem({ as: context?.as }), className)}>
+    <li {...rest} className={twMerge('tw:relative tw:h-fit tw:w-full', className)}>
       {children}
     </li>
   );
--- packages/storybook/stories/react-components/list.stories.tsx
+++ packages/storybook/stories/react-components/list.stories.tsx
@@ -50,29 +50,65 @@ type Story = StoryObj<typeof List>;
 export const UnorderedList: Story = {
   args: {
     as: 'ul',
-    children: (
-      <>
-        <ListItem>Example List Item 1</ListItem>
-        <ListItem>Example List Item 2</ListItem>
-        <ListItem>Example List Item 3</ListItem>
-        <ListItem>Example List Item 4</ListItem>
-        <ListItem>Example List Item 5</ListItem>
-      </>
-    ),
   },
+  render: ({ ...args }) => (
+    <List {...args}>
+      <ListItem>Example List Item 1</ListItem>
+      <ListItem>Example List Item 2</ListItem>
+      <ListItem>Example List Item 3</ListItem>
+      <ListItem>Example List Item 4</ListItem>
+      <ListItem>Example List Item 5</ListItem>
+    </List>
+  ),
 };
 
 export const OrderedList: Story = {
   args: {
     as: 'ol',
-    children: (
-      <>
-        <ListItem>Example List Item 1</ListItem>
-        <ListItem>Example List Item 2</ListItem>
-        <ListItem>Example List Item 3</ListItem>
-        <ListItem>Example List Item 4</ListItem>
-        <ListItem>Example List Item 5</ListItem>
-      </>
-    ),
   },
+  render: ({ ...args }) => (
+    <List {...args}>
+      <ListItem>Example List Item 1</ListItem>
+      <ListItem>Example List Item 2</ListItem>
+      <ListItem>Example List Item 3</ListItem>
+      <ListItem>Example List Item 4</ListItem>
+      <ListItem>Example List Item 5</ListItem>
+    </List>
+  ),
+};
+
+export const NestedUnorderedList: Story = {
+  args: {
+    as: 'ul',
+  },
+  render: ({ ...args }) => (
+    <List {...args}>
+      <ListItem>Example List Item 1</ListItem>
+      <ListItem>Example List Item 2</ListItem>
+      <ListItem>Example List Item 3</ListItem>
+      <List as="ul">
+        <ListItem>Example Nested List Item 1</ListItem>
+        <ListItem>Example Nested List Item 2</ListItem>
+      </List>
+      <ListItem>Example List Item 6</ListItem>
+    </List>
+  ),
+};
+
+export const NestedOrderedList: Story = {
+  args: {
+    as: 'ol',
+  },
+  render: ({ ...args }) => (
+    <List {...args}>
+      <ListItem>Example List Item 1</ListItem>
+      <ListItem>Example List Item 2</ListItem>
+      <ListItem>Example List Item 3</ListItem>
+      <List as="ol">
+        <ListItem>Example Nested List Item 1</ListItem>
+        <ListItem>Example Nested List Item 2</ListItem>
+      </List>
+      <ListItem>Example List Item 6</ListItem>
+    </List>
+  ),
 };
