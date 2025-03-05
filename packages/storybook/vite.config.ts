134b7597332ccb10965aac80782c3893d60c7a77
Raza Mahmood <razammahmood@gmail.com>
2025-03-04 21:16:17 -0500

    Testing storybook for web-components

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    svelte({
+      extensions: ['.svelte', '.svg'],
+      preprocess: vitePreprocess(),
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+    tailwindcss(),
+  ],
 });
