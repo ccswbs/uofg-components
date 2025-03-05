00a6a18b19effa669137af0eb0f19d00a41f61c5
Raza Mahmood <razammahmood@gmail.com>
2025-03-05 00:42:29 -0500

    feat: refactor Divider component to use tailwind-variants for improved styling

--- packages/react-components/src/components/divider/divider.tsx
+++ packages/react-components/src/components/divider/divider.tsx
@@ -1,6 +1,34 @@
+import { tv } from 'tailwind-variants';
+
 /**
  * Separates content on a page visually
  */
-export const Divider = () => (
-  <hr className="uofg-divider tw:mx-auto tw:my-7 tw:h-1 tw:w-3/4 tw:border-0 tw:bg-[linear-gradient(90deg,_var(--tw-color-black)_0%,_var(--tw-color-black)_33%,_var(--tw-color-red)_33%,_var(--tw-color-red)_66%,_var(--tw-color-yellow)_66%,_var(--tw-color-yellow)_100%)]" />
-);
+export const Divider = () => {
+  const divider = tv({
+    slots: {
+      base: 'tw:mx-auto tw:my-7 tw:h-1 tw:w-3/4 tw:border-0 tw:flex',
+      red: 'tw:bg-red',
+      yellow: 'tw:bg-yellow',
+      black: 'tw:bg-black',
+      hr: 'tw:border-0'
+    },
+    compoundSlots: [
+      {
+        slots: ['red', 'yellow', 'black'],
+        class: 'tw:flex-1 tw:h-full'
+      }
+    ]
+  });
+
+  const { base, red, yellow, black, hr } = divider();
+
+  return(
+  <>
+    <div className={base()}>
+      <div className={black()}></div>
+      <div className={red()}></div>
+      <div className={yellow()}></div>
+    </div>
+    <hr className={hr()} />
+  </>
+)};
