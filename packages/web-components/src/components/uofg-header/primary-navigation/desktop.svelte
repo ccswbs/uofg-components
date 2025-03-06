c0c987fc6f3290ad14086c7b38cdec7874edad74
Raza Mahmood <razammahmood@gmail.com>
2025-03-05 22:36:34 -0500

    fix: correct text color class for desktop component links

--- packages/web-components/src/components/uofg-header/primary-navigation/desktop.svelte
+++ packages/web-components/src/components/uofg-header/primary-navigation/desktop.svelte
@@ -8,7 +8,7 @@
     <li>
       {#if item.icon}
         <a
-          class="text-yellow-text flex h-full items-center justify-center border-0 border-b-8 border-solid border-transparent p-4 pt-8 pb-6 transition-colors hover:border-yellow hover:text-white [&>svg]:fill-current"
+          class="text-yellow flex h-full items-center justify-center border-0 border-b-8 border-solid border-transparent p-4 pt-8 pb-6 transition-colors hover:border-yellow hover:text-white [&>svg]:fill-current"
           href={item.href}
           aria-label={item.text}
         >
@@ -16,7 +16,7 @@
         </a>
       {:else}
         <a
-          class="hover:text-yellow-text flex h-full items-center justify-center border-0 border-b-8 border-solid border-transparent p-4 pt-6 text-center transition-colors hover:border-yellow"
+          class="hover:text-yellow flex h-full items-center justify-center border-0 border-b-8 border-solid border-transparent p-4 pt-6 text-center transition-colors hover:border-yellow"
           href={item.href}
         >
           {item.text}
