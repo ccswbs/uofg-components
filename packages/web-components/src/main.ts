d3d16a3ee7ce14004270c876a83dd5038af43762
Raza Mahmood <razammahmood@gmail.com>
2025-03-07 01:35:55 -0500

    feat: add main entry point with global component imports and styles

---
+++ packages/web-components/src/main.ts
@@ -0,0 +1,8 @@
+import UofGAlert from '@/components/uofg-alert.svelte';
+import UofGBackToTop from '@/components/uofg-back-to-top.svelte';
+import UofGFooter from '@/components/uofg-footer/uofg-footer.svelte';
+import UofGHeader from '@/components/uofg-header/uofg-header.svelte';
+import UofGModal from '@/components/uofg-modal.svelte';
+import '@/styles/global.css';
+
+export default { UofGHeader, UofGFooter, UofGModal, UofGAlert, UofGBackToTop };
