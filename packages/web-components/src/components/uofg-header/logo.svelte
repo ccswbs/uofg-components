b4dd99f311c528e051512ca34d20366b2ee5f3a9
Raza Mahmood <razammahmood@gmail.com>
2025-03-11 13:14:43 -0400

    refactor: simplify logo component by removing tailwind-merge and optimizing class bindings

--- packages/web-components/src/components/uofg-header/logo.svelte
+++ packages/web-components/src/components/uofg-header/logo.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { twJoin } from 'tailwind-merge';
   import Decorative from '../../svg/decorative.svelte';
   import LogoSmall from '../../svg/logo-small.svelte';
   import Logo from '../../svg/logo.svelte';
@@ -13,32 +12,22 @@
     slots: {
       base: 'flex w-fit',
       decoration: 'left-0 h-full w-[7.5rem] min-[1320px]:absolute [&>svg]:block [&>svg]:h-full [&>svg]:w-auto',
-      logo: 'flex h-full w-fit items-center transition-opacity hover:opacity-75 focus:opacity-75 min-[1320px]:absolute min-[1320px]:left-[max(calc((100%-1320px)/2),7.5rem)] [&>svg]:block [&>svg]:w-auto',
+      logo: 'flex h-full w-fit items-center transition-opacity hover:opacity-75 focus:opacity-75 min-[1320px]:absolute min-[1320px]:left-[max(calc((100%-1320px)/2),7.5rem)] [&>svg]:block [&>svg]:h-full [&>svg]:w-auto',
     },
   });
+
+  const { base, decoration, logo } = classes();
 </script>
 
-<div class="flex w-fit">
+<div class={base()}>
   {#if $headerState?.mode === 'desktop'}
     <!-- Decorative SVG -->
-    <div
-      class="left-0 h-full w-[7.5rem] min-[1320px]:absolute [&>svg]:block [&>svg]:h-full [&>svg]:w-auto"
-      aria-hidden="true"
-    >
+    <div class={decoration()} aria-hidden="true">
       <Decorative />
     </div>
   {/if}
 
-  <a
-    class={twJoin(
-      'flex h-full w-fit items-center transition-opacity hover:opacity-75 focus:opacity-75 min-[1320px]:absolute min-[1320px]:left-[max(calc((100%-1320px)/2),7.5rem)] [&>svg]:block [&>svg]:w-auto',
-      $headerState?.mode === 'desktop' ? '[&>svg]:h-22'
-      : $headerState?.variant === 'dual-brand' ? 'px-2 [&>svg]:h-[7.5rem]'
-      : '[&>svg]:h-[5rem]',
-    )}
-    href="https://www.uoguelph.ca"
-    aria-label="University of Guelph Home Page"
-  >
+  <a class={logo()} href="https://www.uoguelph.ca" aria-label="University of Guelph Home Page">
     {#if $headerState?.mode === 'desktop'}
       <Logo />
     {:else}
