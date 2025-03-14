56ccb988bd5ba0a7c171d290d591e63bd07732db
Raza Mahmood <razammahmood@gmail.com>
2025-03-14 14:26:52 -0400

    refactor: enhance menu component structure and add transition effects for improved user experience

--- packages/web-components/src/components/uofg-header/sub-navigation/desktop.svelte
+++ packages/web-components/src/components/uofg-header/sub-navigation/desktop.svelte
@@ -1,7 +1,10 @@
-<script>
-  import Menu from '../../../lib/menu.svelte';
+<script lang="ts">
+  import Menu from '../../../lib/menu/menu.svelte';
+  import MenuButton from '../../../lib/menu/menu-button.svelte';
   import FontAwesomeIcon from '../../../lib/font-awesome-icon.svelte';
   import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+  import { slide } from 'svelte/transition';
+  import { cubicInOut } from 'svelte/easing';
 
   let { items } = $props();
 </script>
@@ -9,30 +12,34 @@
 {#each items as item (item.text)}
   {#if 'items' in item}
     <li>
-      <Menu
-        class="relative h-full"
-        buttonClass="flex h-full items-center justify-center gap-2 px-2 transition-colors hover:bg-yellow focus:bg-yellow aria-expanded:bg-yellow hover:text-yellow-contrast focus:text-yellow-contrast aria-expanded:text-yellow-contrast [&_svg]:aria-expanded:rotate-180"
-        contentClass="absolute right-0 top-full z-50 flex min-w-[20rem] flex-col bg-grey-muted [&>li]:contents"
-        as="ul"
-      >
+      <Menu class="relative h-full">
         {#snippet button()}
-          <span class="flex items-center gap-2 [&>svg]:transition-transform">
-            <span>{item.text}</span>
-            <FontAwesomeIcon icon={faChevronDown} />
-          </span>
+          <MenuButton
+            class="flex h-full items-center justify-center gap-2 px-2 transition-colors hover:bg-yellow hover:text-yellow-contrast focus:bg-yellow focus:text-yellow-contrast aria-expanded:bg-yellow aria-expanded:text-yellow-contrast [&_svg]:aria-expanded:rotate-180"
+          >
+            <span class="flex items-center gap-2 [&>svg]:transition-transform">
+              <span>{item.text}</span>
+              <FontAwesomeIcon icon={faChevronDown} />
+            </span>
+          </MenuButton>
         {/snippet}
 
-        {#each item.items as link}
-          <li>
-            <a
-              {...link.attributes}
-              class={`border-grey/50 border-0 border-b border-solid p-2 transition-colors hover:bg-yellow hover:text-yellow-contrast focus:bg-yellow focus:text-yellow-contrast ${link.attributes?.class ?? ''}`}
-              href={link.href}
-            >
-              {link.text}
-            </a>
-          </li>
-        {/each}
+        <ul
+          class="bg-grey-muted absolute top-full right-0 z-50 flex min-w-[20rem] flex-col [&>li]:contents"
+          transition:slide={{ duration: 200, easing: cubicInOut }}
+        >
+          {#each item.items as menuItem}
+            <li>
+              <a
+                {...menuItem.attributes}
+                class={`border-grey/50 border-0 border-b border-solid p-2 transition-colors hover:bg-yellow hover:text-yellow-contrast focus:bg-yellow focus:text-yellow-contrast ${menuItem.attributes?.class ?? ''}`}
+                href={menuItem.href}
+              >
+                {menuItem.text}
+              </a>
+            </li>
+          {/each}
+        </ul>
       </Menu>
     </li>
   {:else}
