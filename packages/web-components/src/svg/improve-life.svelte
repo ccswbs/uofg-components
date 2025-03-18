99b84abcf19e66ca93f8581cbfec4d9067702f7e
Raza Mahmood <razammahmood@gmail.com>
2025-03-18 13:47:59 -0400

    refactor: format SVG files for improved readability and consistency

--- packages/web-components/src/svg/improve-life.svelte
+++ packages/web-components/src/svg/improve-life.svelte
@@ -3,27 +3,28 @@
   <defs>
     <style>
       .st0 {
-      fill: #fff;
-      font-family: MyriadPro-Regular, 'Myriad Pro';
-      font-size: 44.3px;
+        fill: #fff;
+        font-family: MyriadPro-Regular, 'Myriad Pro', sans-serif;
+        font-size: 44.3px;
       }
 
-      .st0, .st1 {
-      isolation: isolate;
+      .st0,
+      .st1 {
+        isolation: isolate;
       }
 
       .st2 {
-      fill: #221f20;
+        fill: #221f20;
       }
 
       .st3 {
-      fill: #e51937;
+        fill: #e51937;
       }
     </style>
   </defs>
   <g>
-    <rect class="st2" x="1.5" y="1.5" width="715.9" height="76.5"/>
-    <path class="st3" d="M715.9,3v73.5H3V3h712.9M718.9,0H0v79.5h718.9V0h0Z"/>
+    <rect class="st2" x="1.5" y="1.5" width="715.9" height="76.5" />
+    <path class="st3" d="M715.9,3v73.5H3V3h712.9M718.9,0H0v79.5h718.9V0h0Z" />
   </g>
   <g class="st1">
     <text class="st0" transform="translate(107.3 54.3)"><tspan x="0" y="0">IMP</tspan></text>
--- packages/web-components/src/svg/logo-small.svelte
+++ packages/web-components/src/svg/logo-small.svelte
@@ -2,19 +2,19 @@
   <defs>
     <style>
       .cls-1 {
-      fill: #fff;
-      font-family: MyriadPro-Regular, 'Myriad Pro';
-      font-size: 44.28px;
+        fill: #fff;
+        font-family: MyriadPro-Regular, 'Myriad Pro', sans-serif;
+        font-size: 44.28px;
       }
 
       .cls-2 {
-      fill: #e51937;
+        fill: #e51937;
       }
     </style>
   </defs>
   <g>
-    <rect x="83.14" y="42.23" width="180.66" height="352.2"/>
-    <path class="cls-2" d="M262.3,43.73v349.2H84.64V43.73h177.66M265.3,40.73H81.64v355.2h183.66V40.73h0Z"/>
+    <rect x="83.14" y="42.23" width="180.66" height="352.2" />
+    <path class="cls-2" d="M262.3,43.73v349.2H84.64V43.73h177.66M265.3,40.73H81.64v355.2h183.66V40.73h0Z" />
   </g>
   <text class="cls-1" transform="translate(112.91 242.36)"><tspan x="0" y="0">CREST</tspan></text>
 </svg>
