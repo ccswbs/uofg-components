094acfb0d08ffd276f34568f24c6b6e3456c1299
Raza Mahmood <razammahmood@gmail.com>
2025-03-19 14:45:46 -0400

    fix: update play button styles for improved hover and focus effects

--- packages/react-components/src/components/embedded-video/embedded-video-modal-button.tsx
+++ packages/react-components/src/components/embedded-video/embedded-video-modal-button.tsx
@@ -17,7 +17,7 @@ export function EmbeddedVideoModalButton({ type, children, className }: Embedded
 
   if (type === 'play-button') {
     const button = twMerge(
-      'uog:flex uog:aspect-square uog:w-24 uog:items-center uog:justify-center uog:rounded-full uog:bg-black/30 uog:text-4xl uog:text-white uog:transition-colors uog:hover:bg-red/30 uog:focus:bg-red/30',
+      'uog:flex uog:aspect-square uog:w-24 uog:items-center uog:justify-center uog:rounded-full uog:bg-red uog:text-4xl uog:text-white uog:transition-colors uog:hover:bg-red-focus uog:focus:bg-red-focus',
       className,
     );
 
