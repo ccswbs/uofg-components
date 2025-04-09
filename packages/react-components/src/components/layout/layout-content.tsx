da1524efc2589ae53c1527191b79b75fa5112e02
Raza Mahmood <razammahmood@gmail.com>
2025-04-09 01:11:57 -0400

    feat: add Layout and LayoutContent components with loading state support

---
+++ packages/react-components/src/components/layout/layout-content.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Container } from '../container/container';
+
+export type LayoutContentProps = PropsWithChildren<{
+  /** Additional classes to apply to the component. */
+  className?: string;
+  /**
+   * Whether the content should be rendered as a Container component.
+   *
+   * @default true
+   */
+  container?: boolean;
+}>;
+
+export function LayoutContent({ children, className, container = true }: LayoutContentProps) {
+  const classes = twMerge('uofg-layout-content uog:pb-4 uog:flex-1', className);
+
+  if (container) {
+    return (
+      <Container id="content" as="main" className={classes}>
+        {children}
+      </Container>
+    );
+  }
+
+  return (
+    <main id="content" className={classes}>
+      {children}
+    </main>
+  );
+}
