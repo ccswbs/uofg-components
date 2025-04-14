586c27c316085f92ce42023976e42ec6164e4525
Raza Mahmood <razammahmood@gmail.com>
2025-04-14 16:47:05 -0400

    fix: update TabList component to support vertical layout on smaller screens

--- packages/react-components/src/components/tabs/tab-list.tsx
+++ packages/react-components/src/components/tabs/tab-list.tsx
@@ -8,7 +8,10 @@ export type TabListProps = PropsWithChildren<{
 }>;
 
 export function TabList({ children, className }: TabListProps) {
-  const classes = twMerge('uofg-tab-list uog:flex uog:gap-1 uog:border-b-4 uog:border-yellow uog:w-full', className);
+  const classes = twMerge(
+    'uofg-tab-list uog:flex uog:gap-1 uog:border-b-4 uog:border-yellow uog:w-full uog:flex-col uog:md:flex-row',
+    className,
+  );
   return <HUITabList className={classes}>{children}</HUITabList>;
 }
 
