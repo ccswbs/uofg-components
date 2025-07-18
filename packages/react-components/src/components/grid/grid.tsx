020d083e5e69acfb100fbd57fa557cff16420dc3
Raza Mahmood <razammahmood@gmail.com>
2025-07-18 01:08:12 -0400

    feat(grid): enhance `Grid` component with advanced responsive templates and new story
    - Updated `Grid` props to support dynamic responsive templates using breakpoints.
    - Improved `gap` flexibility to accept both string and number values.
    - Added Storybook story to demonstrate the enhanced `Grid` functionality.

--- packages/react-components/src/components/grid/grid.tsx
+++ packages/react-components/src/components/grid/grid.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { tv } from 'tailwind-variants';
 
+type Breakpoints = 'base' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+
 export type GridProps = PropsWithChildren<{
   /** Extra CSS classes to add to the grid container (optional). */
   className?: string;
@@ -9,6 +11,8 @@ export type GridProps = PropsWithChildren<{
   /**
    * Sets whether the template values should be applied to rows or columns of the grid. Use 'row' for row templates and
    * 'column' for column templates.
+   *
+   * @default 'column'
    */
   major?: 'row' | 'column';
 
@@ -24,26 +28,123 @@ export type GridProps = PropsWithChildren<{
    * This works like the CSS grid-template-rows or grid-template-columns properties for responsive layouts.
    */
   template: {
-    breakpoint: 'base' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-    value: number[] | string[];
-  }[];
-
-  alignment: {
+    [K in Breakpoints]?: string[];
+  };
+  alignment?: {
     x: 'start' | 'center' | 'end' | 'stretch';
     y: 'start' | 'center' | 'end' | 'stretch';
   };
-  gap: {
-    x: number;
-    y: number;
+  gap?: {
+    x: number | string;
+    y: number | string;
   };
 }>;
 
-export function Grid({ children, className }: GridProps) {
+export function Grid({ template, major = 'column', gap, children, className }: GridProps) {
   const classes = tv({
-    base: 'grid',
+    base: 'grid gap-x-(--uofg-grid-gap-x) gap-y-(--uofg-grid-gap-y)',
+    variants: {
+      major: { row: '', column: '' },
+      hasBaseTemplate: { true: '' },
+      hasSmTemplate: { true: '' },
+      hasMdTemplate: { true: '' },
+      hasLgTemplate: { true: '' },
+      hasXlTemplate: { true: '' },
+      has2XlTemplate: { true: '' },
+    },
+    compoundVariants: [
+      {
+        hasBaseTemplate: true,
+        major: 'column',
+        class: 'grid-cols-(--uofg-grid-template-column)',
+      },
+      {
+        hasSmTemplate: true,
+        major: 'column',
+        class: 'sm:grid-cols-(--uofg-grid-sm-template-column)',
+      },
+      {
+        hasMdTemplate: true,
+        major: 'column',
+        class: 'md:grid-cols-(--uofg-grid-md-template-column)',
+      },
+      {
+        hasLgTemplate: true,
+        major: 'column',
+        class: 'lg:grid-cols-(--uofg-grid-lg-template-column)',
+      },
+      {
+        hasXlTemplate: true,
+        major: 'column',
+        class: 'xl:grid-cols-(--uofg-grid-xl-template-column)',
+      },
+      {
+        has2XlTemplate: true,
+        major: 'column',
+        class: '2xl:grid-cols-(--uofg-grid-2xl-template-column)',
+      },
+      {
+        hasBaseTemplate: true,
+        major: 'row',
+        class: 'grid-rows-(--uofg-grid-template-row)',
+      },
+      {
+        hasSmTemplate: true,
+        major: 'row',
+        class: 'sm:grid-rows-(--uofg-grid-sm-template-row)',
+      },
+      {
+        hasMdTemplate: true,
+        major: 'row',
+        class: 'md:grid-rows-(--uofg-grid-md-template-row)',
+      },
+      {
+        hasLgTemplate: true,
+        major: 'row',
+        class: 'lg:grid-rows-(--uofg-grid-lg-template-row)',
+      },
+      {
+        hasXlTemplate: true,
+        major: 'row',
+        class: 'xl:grid-rows-(--uofg-grid-xl-template-row)',
+      },
+      {
+        has2XlTemplate: true,
+        major: 'row',
+        class: '2xl:grid-rows-(--uofg-grid-2xl-template-row)',
+      },
+    ],
+  })({
+    major: major,
+    hasBaseTemplate: template.base !== undefined,
+    hasSmTemplate: template.sm !== undefined,
+    hasMdTemplate: template.md !== undefined,
+    hasLgTemplate: template.lg !== undefined,
+    hasXlTemplate: template.xl !== undefined,
+    has2XlTemplate: template['2xl'] !== undefined,
   });
 
-  return <div className={`uofg-grid ${twMerge(classes(), className)}`}>{children}</div>;
+  const vars = Object.entries(template).reduce(
+    (acc, [breakpoint, values]) => {
+      acc[`--uofg-grid-${breakpoint}-template-${major}`] = values.join(' ');
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+
+  return (
+    <div
+      className={`uofg-grid ${twMerge(classes, className)}`}
+      style={{
+        ...vars,
+        // @ts-ignore
+        '--uofg-grid-gap-x': typeof gap?.x === 'string' ? gap?.x : `${gap?.x}px`,
+        '--uofg-grid-gap-y': typeof gap?.y === 'string' ? gap?.y : `${gap?.y}px`,
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
-Grid.displayName = 'Contact';
+Grid.displayName = 'Grid';
---
+++ packages/storybook/stories/react-components/grid.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Card } from '../../../react-components/src/components/card/card';
+import { CardContent } from '../../../react-components/src/components/card/card-content';
+import { CardTitle } from '../../../react-components/src/components/card/card-title';
+import { Container } from '../../../react-components/src/components/container/container';
+import { Grid } from '../../../react-components/src/components/grid/grid';
+
+const config: Meta<typeof Grid> = {
+  title: 'React Components/Grid',
+  component: Grid,
+  parameters: {
+    'layout': 'padded',
+    'docs': {
+      toc: true,
+    },
+    'in-development': true,
+  },
+  tags: ['autodocs'],
+};
+
+export default config;
+
+type Story = StoryObj<typeof Grid>;
+
+export const Default: Story = {
+  args: {
+    template: {
+      base: ['1fr'],
+      sm: ['1fr', '1fr'],
+      md: ['1fr', '1fr', '1fr'],
+      xl: ['1fr', '1fr', '1fr', '1fr'],
+    },
+    gap: {
+      x: 10,
+      y: 10,
+    },
+  },
+  render: ({ ...args }) => {
+    return (
+      <Container>
+        <Grid {...args}>
+          {Array.from({ length: 10 }, (_value, index) => (
+            <Card>
+              <CardContent>
+                <CardTitle>Example Grid Item {index + 1}</CardTitle>
+                <div>
+                  Iusto possimus possimus delectus et. Et aspernatur culpa quis sint at nam voluptatibus. Occaecati
+                  perspiciatis ea eius dolorem aliquid. Ad ducimus aut aspernatur. Cumque enim repellat reprehenderit.
+                  Similique perspiciatis alias doloremque reprehenderit eum. A laboriosam dolore. Facere possimus qui..
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </Grid>
+      </Container>
+    );
+  },
+};
