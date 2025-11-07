4e0f4722eeea34c0fa714db2e4c5288731476665
Raza Mahmood <razammahmood@gmail.com>
2025-11-07 15:43:57 -0500

    feat(back-to-top): enable React Server Components compatibility by adding 'use client' directive

--- packages/react-components/src/components/back-to-top/back-to-top.tsx
+++ packages/react-components/src/components/back-to-top/back-to-top.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { faChevronUp } from '@awesome.me/kit-7993323d0c/icons/classic/solid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
