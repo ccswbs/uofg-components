41f33dc26565e008ec335d1762798e6be6c9d91c
Raza Mahmood <razammahmood@gmail.com>
2025-12-08 14:30:11 -0500

    Ensure address title maintains proper formatting with non-wrapping whitespace classes

--- packages/web-components/src/components/uofg-footer/address.svelte
+++ packages/web-components/src/components/uofg-footer/address.svelte
@@ -12,7 +12,7 @@
 <address
   class="mt-6 grid grid-rows-5 items-center gap-0 not-italic md:col-[2] md:row-[1] md:mt-0 lg:col-[4] lg:row-[1]"
 >
-  <strong>{address.title}</strong>
+  <strong class="text-nowrap whitespace-nowrap">{address.title}</strong>
   <span>{address.street},</span>
   <span>{address.city}, Ontario, Canada</span>
   <span>{address.postalCode}</span>
