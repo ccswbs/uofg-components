cf5d8a42f5be2a8a972db889dc65b9ddeaac0fa6
RazaMM <razammahmood@gmail.com>
2026-04-06 11:31:48 -0400

    Refactor and centralize `uofg-header` data models; integrate dynamic state management for header navigation.

--- packages/web-components/src/components/uofg-header/data/guelph.ts
+++ packages/web-components/src/components/uofg-header/data/guelph.ts
@@ -1,11 +1,12 @@
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import type { HTMLAttributes } from 'svelte/elements';
+import { HeaderData } from './index';
 
 export type HeaderLinkAndMenuCommon = {
   text: string;
   icon?: any;
   highlight?: boolean;
-  excludeFromMainMenu?: boolean;
+  standalone?: boolean;
   attributes?: HTMLAttributes<HTMLAnchorElement>;
 };
 
@@ -37,6 +38,12 @@ export const primaryNavigation: HeaderLink[] = [
   {
     text: 'Student Life',
     href: 'https://www.uoguelph.ca/studentexperience/',
+  },
+  {
+    text: 'Search the Universityd of Guelph',
+    href: 'https://uoguelph.ca/search',
+    standalone: true,
+    icon: faSearch,
   },
 ];
 
@@ -85,6 +92,33 @@ export const topNavigation: (HeaderLink | HeaderMenu)[] = [
     href: 'https://uoguelph.ca/apply/',
     highlight: true,
   },
+  {
+    text: 'Accoundt Menu',
+    icon: faUser,
+    standalone: true,
+    items: [
+      {
+        text: 'U of G Insider',
+        href: 'https://uoguelphca.sharepoint.com/',
+      },
+      {
+        text: 'WebAdvisor',
+        href: 'https://www.uoguelph.ca/webadvisor/',
+      },
+      {
+        text: 'GryphMail',
+        href: 'https://mail.uoguelph.ca/',
+      },
+      {
+        text: 'CourseLink',
+        href: 'https://courselink.uoguelph.ca/',
+      },
+      {
+        text: 'GryphLife',
+        href: 'https://gryphlife.uoguelph.ca/',
+      },
+    ],
+  },
 ];
 
 export const accountMenu: HeaderMenu = {
@@ -112,4 +146,103 @@ export const accountMenu: HeaderMenu = {
     },
   ],
   icon: faUser,
+};
+
+export const guelph: HeaderData = {
+  id: 'guelph',
+  top: [
+    {
+      text: 'Give',
+      href: 'https://bbis.alumni.uoguelph.ca/BBIS_Cannon/give/uofg',
+    },
+    {
+      text: 'News',
+      href: 'https://news.uoguelph.ca/',
+    },
+    {
+      text: 'Quick Links',
+      items: [
+        {
+          text: 'Future Students',
+          href: 'https://www.uoguelph.ca/admission/',
+        },
+        {
+          text: 'Current Students',
+          href: 'https://www.uoguelph.ca/students',
+        },
+        {
+          text: 'Alumni & Donors',
+          href: 'https://alumni.uoguelph.ca/',
+        },
+        {
+          text: 'Faculty & Staff',
+          href: 'https://www.uoguelph.ca/faculty',
+        },
+        {
+          text: 'Employers & Partners',
+          href: 'https://cecs.uoguelph.ca/employers-institutions',
+        },
+      ],
+    },
+    {
+      text: 'Apply Now',
+      href: 'https://uoguelph.ca/apply/',
+      highlight: true,
+    },
+    {
+      text: 'Account Menu',
+      icon: faUser,
+      standalone: true,
+      items: [
+        {
+          text: 'U of G Insider',
+          href: 'https://uoguelphca.sharepoint.com/',
+        },
+        {
+          text: 'WebAdvisor',
+          href: 'https://www.uoguelph.ca/webadvisor/',
+        },
+        {
+          text: 'GryphMail',
+          href: 'https://mail.uoguelph.ca/',
+        },
+        {
+          text: 'CourseLink',
+          href: 'https://courselink.uoguelph.ca/',
+        },
+        {
+          text: 'GryphLife',
+          href: 'https://gryphlife.uoguelph.ca/',
+        },
+      ],
+    },
+  ],
+  primary: [
+    {
+      text: 'About',
+      href: 'https://uoguelph.ca/about',
+    },
+    {
+      text: 'Academics',
+      href: 'https://www.uoguelph.ca/programs/undergraduate',
+    },
+    {
+      text: 'Admission',
+      href: 'https://uoguelph.ca/admissions',
+    },
+    {
+      text: 'Research',
+      href: 'https://uoguelph.ca/research',
+    },
+    {
+      text: 'Student Life',
+      href: 'https://www.uoguelph.ca/studentexperience/',
+    },
+    {
+      text: 'Search the University of Guelph',
+      href: 'https://uoguelph.ca/search',
+      standalone: true,
+      icon: faSearch,
+    },
+  ],
 };
---
+++ packages/web-components/src/components/uofg-header/data/index.ts
@@ -0,0 +1,31 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import type { HTMLAttributes } from 'svelte/elements';
+import { guelph } from './guelph';
+
+export type HeaderLinkAndMenuCommon = {
+  text: string;
+  icon?: IconDefinition;
+  highlight?: boolean;
+  standalone?: boolean;
+  attributes?: HTMLAttributes<HTMLAnchorElement>;
+};
+
+export type HeaderLink = {
+  href: string;
+} & HeaderLinkAndMenuCommon;
+
+export type HeaderMenu = {
+  items: HeaderLink[];
+} & HeaderLinkAndMenuCommon;
+
+export type HeaderData = {
+  id: string;
+  homepage?: {
+    url: string;
+    text: string;
+  };
+  top?: (HeaderLink | HeaderMenu)[];
+  primary: HeaderLink[];
+};
+
+export const variants: HeaderData[] = [guelph];
